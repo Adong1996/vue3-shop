598c1e3e9b870e4cdcf090d2562926ad9d89cf24
chendong <646392218@qq.com>
2021-07-25 18:40:10 +0800

    login

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import {store} from './store/index.js'
-import VueI18n from 'vue-i18n'
+// import VueI18n from 'vue-i18n'
 import 'lib-flexible'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
-app.use(VueI18n)
+// app.use(VueI18n)
 app.mount('#app')
